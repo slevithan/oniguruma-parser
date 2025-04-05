e8eb8295648bf3e0f32e209e94c56ced602e1e23
Steven Levithan <steves_list@hotmail.com>
2025-04-05 17:44:21 +0200

    Tests: replaceWith

--- test/spec-utils.ts
+++ test/spec-utils.ts
@@ -1,10 +1,8 @@
 import type {OptimizationName} from '../dist/optimizer/optimizations.js';
 import type {AlternativeElementNode, OnigurumaAst} from '../dist/parser/parse.js';
-import type {Visitor} from '../dist/traverser/traverse.js';
 import {generate} from '../dist/generator/generate.js';
 import {toOnigurumaAst} from '../dist/index.js'
 import {getOptionalOptimizations, optimize} from '../dist/optimizer/optimize.js';
-import {traverse} from '../dist/traverser/traverse.js';
 
 function gen(pattern: string): string {
   return generate(toOnigurumaAst(pattern)).pattern;
--- test/traverser/path-methods.test.ts
+++ test/traverser/path-methods.test.ts
@@ -1,13 +1,36 @@
-import type {GroupNode} from '../../dist/parser/parse.js';
+import type {CharacterNode, CharacterSetNode, GroupNode} from '../../dist/parser/parse.js';
 import type {Visitor} from '../../dist/traverser/traverse.js';
 import {toOnigurumaAst} from '../../dist/index.js';
-import {createCharacter} from '../../dist/parser/parse.js';
+import {createCharacter, createCharacterSet} from '../../dist/parser/parse.js';
 import {traverse} from '../../dist/traverser/traverse.js';
 import {cpOf} from '../../dist/utils.js';
 import {singleAltAst} from '../spec-utils.js';
 import {describe, expect, it} from 'vitest';
 
 describe('Traverser: path', () => {
+  describe('replaceWith', () => {
+    it('should replace a node with a new node', () => {
+      const visitor: Visitor = {
+        Character({node, replaceWith}) {
+          const {value} = node as CharacterNode;
+          if (value === cpOf('a')) {
+            replaceWith(createCharacterSet('dot'));
+          }
+        },
+        CharacterSet({node, replaceWith}) {
+          const {kind} = node as CharacterSetNode;
+          if (kind === 'dot') {
+            replaceWith(createCharacter(cpOf('a')));
+          }
+        },
+      };
+      expect(traverse(toOnigurumaAst('a.'), visitor)).toEqual(singleAltAst([
+        createCharacterSet('dot'),
+        createCharacter(cpOf('a')),
+      ]));
+    });
+  });
+
   describe('replaceWithMultiple', () => {
     it('should replace a node with multiple nodes', () => {
       const visitor: Visitor = {
@@ -27,4 +50,6 @@ describe('Traverser: path', () => {
       ]));
     });
   });
+
+  // TODO: Add tests for the remaining path methods
 });
