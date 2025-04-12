6f7adcefd4655104a74c194f1c5771c2805eba68
Steven Levithan <steves_list@hotmail.com>
2025-04-12 21:41:46 +0200

    Code tidy

--- test/parser/parse-quantifier.test.ts
+++ test/parser/parse-quantifier.test.ts
@@ -1,43 +1,32 @@
-import type {NodeQuantifierKind, QuantifiableNode, QuantifierNode} from '../../dist/parser/parse.js';
+import type {AlternativeElementNode, NodeQuantifierKind, QuantifiableNode} from '../../dist/parser/parse.js';
 import {createCharacterSet, createQuantifier, parse} from '../../dist/parser/parse.js';
 import {r} from '../../dist/utils.js';
 import {singleAltAst} from '../spec-utils.js';
 import {describe, expect, it} from 'vitest';
 
-describe('Parser: Quantifier', () => {
-  function star(body: QuantifiableNode, kind?: NodeQuantifierKind): QuantifierNode {
-    return createQuantifier(kind ?? 'greedy', 0, Infinity, body);
-  }
-  function qMark(body: QuantifiableNode, kind?: NodeQuantifierKind): QuantifierNode {
-    return createQuantifier(kind ?? 'greedy', 0, 1, body);
-  }
+const dot = createCharacterSet('dot');
+const qMark = (body: QuantifiableNode, kind: NodeQuantifierKind = 'greedy') =>
+  createQuantifier(kind, 0, 1, body);
+const star = (body: QuantifiableNode, kind: NodeQuantifierKind = 'greedy') =>
+  createQuantifier(kind, 0, Infinity, body);
+const single = (node: AlternativeElementNode) => singleAltAst([node]);
 
+describe('Parser: Quantifier', () => {
   it('should parse quantifiers', () => {
-    expect(parse('.*')).toEqual(singleAltAst([
-      star(createCharacterSet('dot')),
-    ]));
+    expect(parse('.*')).toEqual(single(star(dot)));
   });
 
   it('should parse quantifier chains', () => {
-    expect(parse('.**')).toEqual(singleAltAst([
-      star(star(createCharacterSet('dot'))),
-    ]));
+    expect(parse('.**')).toEqual(single(star(star(dot))));
   });
 
   it('should parse reversed (possessive) interval quantifiers', () => {
-    expect(parse('.{1,0}')).toEqual(singleAltAst([
-      createQuantifier('possessive', 0, 1, createCharacterSet('dot')),
-    ]));
+    const possessive0To1 = createQuantifier('possessive', 0, 1, dot);
+    expect(parse('.{1,0}')).toEqual(single(possessive0To1));
     // Special case: `?` isn't a lazy-suffix
-    expect(parse('.{1,0}?')).toEqual(singleAltAst([
-      qMark(createQuantifier('possessive', 0, 1, createCharacterSet('dot'))),
-    ]));
-    expect(parse('.{1,0}??')).toEqual(singleAltAst([
-      qMark(createQuantifier('possessive', 0, 1, createCharacterSet('dot')), 'lazy'),
-    ]));
-    expect(parse('.{1,0}?+')).toEqual(singleAltAst([
-      qMark(createQuantifier('possessive', 0, 1, createCharacterSet('dot')), 'possessive'),
-    ]));
+    expect(parse('.{1,0}?')).toEqual(single(qMark(possessive0To1)));
+    expect(parse('.{1,0}??')).toEqual(single(qMark(possessive0To1, 'lazy')));
+    expect(parse('.{1,0}?+')).toEqual(single(qMark(possessive0To1, 'possessive')));
   });
 
   it('should throw when quantifying an unquantifiable node', () => {
@@ -49,6 +38,7 @@ describe('Parser: Quantifier', () => {
       '(?i)*',
       r`\K*`,
       '(?=)*',
+      '(?<=)*',
       '(*FAIL)*',
     ];
     for (const input of cases) {
